44b83f0a6cc8b6ec9737281a0a32e68c5dbe86a2
Pablo Candela <pabloccandela@gmail.com>
2023-04-01 12:18:01 -0300

    arreglos en home

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -4,6 +4,7 @@ module.exports = {
     "password": process.env.DB_PASSWORD || "SJTEsJPVB1OT1w7hoZvG",
     "database": process.env.DB_NAME || "railway",
     "host": process.env.DB_HOST || "containers-us-west-123.railway.app",
+    "port": process.env.DB_PORT || "7940",
     "dialect": "mysql"
     // "username": "moondb",
     // "password": "HUG@7Wv8UKj6vGU",
@@ -16,6 +17,7 @@ module.exports = {
     "password": process.env.DB_PASSWORD || "SJTEsJPVB1OT1w7hoZvG",
     "database": process.env.DB_NAME || "railway",
     "host": process.env.DB_HOST || "containers-us-west-123.railway.app",
+    "port": process.env.DB_PORT || "7940",
     "dialect": "mysql"
   },
   "production": {
