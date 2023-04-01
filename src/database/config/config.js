4ceae569ca29bffc2997ea4364b5d6971933ea84
Pablo Candela <pabloccandela@gmail.com>
2023-03-31 23:12:28 -0300

    DATA BASE

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,23 +1,28 @@
 module.exports = {
   "development": {
-    "username": "moondb",
-    "password": "HUG@7Wv8UKj6vGU",
-    "database": "moondb_shoes",
-    "host": "mysql-moondb.alwaysdata.net",
+    "username": process.env.DB_USER || "root",
+    "password": process.env.DB_PASSWORD || "SJTEsJPVB1OT1w7hoZvG",
+    "database": process.env.DB_NAME || "railway",
+    "host": process.env.DB_HOST || "containers-us-west-123.railway.app",
     "dialect": "mysql"
+    // "username": "moondb",
+    // "password": "HUG@7Wv8UKj6vGU",
+    // "database": "moondb_shoes",
+    // "host": "mysql-moondb.alwaysdata.net",
+    // "dialect": "mysql"
   },
   "test": {
-    "username": "moondb_flyio",
-    "password": "HUG@7Wv8UKj6vGU",
-    "database": "moondb_shoes",
-    "host": "mysql-moondb.alwaysdata.net",
+    "username": "root",
+    "password": "SJTEsJPVB1OT1w7hoZvG",
+    "database": "railway",
+    "host": "containers-us-west-123.railway.app",
     "dialect": "mysql"
   },
   "production": {
-    "username": "moondb_flyio",
-    "password": "HUG@7Wv8UKj6vGU",
-    "database": "moondb_shoes",
-    "host": "mysql-moondb.alwaysdata.net",
+    "username": "root",
+    "password": "SJTEsJPVB1OT1w7hoZvG",
+    "database": "railway",
+    "host": "containers-us-west-123.railway.app",
     "dialect": "mysql"
   }
 }
