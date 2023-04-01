908530811ccbc5150357e13c81c697c6b475334d
Pablo Candela <pabloccandela@gmail.com>
2023-03-31 23:26:10 -0300

    DATA BASE

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -12,17 +12,18 @@ module.exports = {
     // "dialect": "mysql"
   },
   "test": {
-    "username": "root",
-    "password": "SJTEsJPVB1OT1w7hoZvG",
-    "database": "railway",
-    "host": "containers-us-west-123.railway.app",
+    "username": process.env.DB_USER || "root",
+    "password": process.env.DB_PASSWORD || "SJTEsJPVB1OT1w7hoZvG",
+    "database": process.env.DB_NAME || "railway",
+    "host": process.env.DB_HOST || "containers-us-west-123.railway.app",
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": "SJTEsJPVB1OT1w7hoZvG",
-    "database": "railway",
-    "host": "containers-us-west-123.railway.app",
+    "username": process.env.DB_USER || "root",
+    "password": process.env.DB_PASSWORD || "SJTEsJPVB1OT1w7hoZvG",
+    "database": process.env.DB_NAME || "railway",
+    "host": process.env.DB_HOST || "containers-us-west-123.railway.app",
+    "port": process.env.DB_PORT || "7940",
     "dialect": "mysql"
   }
 }
