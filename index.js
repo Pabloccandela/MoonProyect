da34cce0ec219eaaff12e2fd72a81d3ffdb11009
Pablo Candela <pabloccandela@gmail.com>
2023-03-31 21:19:41 -0300

    changes in app

--- index.js
+++ index.js
@@ -7,22 +7,22 @@ const path = require('path');
 // const cookieParser = require('cookie-parser');
  
 //port initialization
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port,() => {
     console.log('start listening on http://localhost:' + port);
 });
 
 //middleware
-app.use(express.static(path.join(__dirname,'../public')));
+app.use(express.static(path.join(__dirname,'./public')));
 // app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
 //ejs MVC
 app.set('view engine', 'ejs');
-app.set('views',path.join(__dirname,'./views'));
+app.set('views',path.join(__dirname,'./src/views'));
 
 //Routes
-const routes = require('./routes/mainRoutes');
+const routes = require('./src/routes/mainRoutes');
 app.use('/',routes)
 
--- src/database/config/config.js
+++ src/database/config/config.js
@@ -7,17 +7,17 @@ module.exports = {
     "dialect": "mysql"
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
+    "username": "moondb_flyio",
+    "password": "HUG@7Wv8UKj6vGU",
+    "database": "moondb_shoes",
+    "host": "mysql-moondb.alwaysdata.net",
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": "moondb_flyio",
+    "password": "HUG@7Wv8UKj6vGU",
+    "database": "moondb_shoes",
+    "host": "mysql-moondb.alwaysdata.net",
     "dialect": "mysql"
   }
 }
